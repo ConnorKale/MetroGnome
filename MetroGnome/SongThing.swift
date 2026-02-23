690cf69e83019aab2cf268017989ac8f6c3d5dc9
School Connor Kale <144173365+ConnorKale@users.noreply.github.com>
2026-02-23 10:02:50 -0800

    Updating the JSON
    This will break the reader until I change the name to fileName

--- MetroGnome/SongThing.swift
+++ MetroGnome/SongThing.swift
@@ -8,8 +8,11 @@
 import Foundation // idk if this is necessary
 
 struct Song: Identifiable, Codable {
-    let id: Int // maybe UUID instead?
-    let name: String
-    let tempo: Float
-    let fileExtension: String
+    let id: Int // maybe UUID instead? // Each song's "number"
+    let fileName: String //The file path/name
+    let songName: String // The user-facing name, could appear in the ButtonsView
+    let tempo: Float // The tempo. A faster tempo means it will play slower, enter half the actual tempo to make it play at double time
+    let fileExtension: String // To decide whether to use the MP3 decoder or not
+    let artist: String // The musician/composer
+    let description: String // Optional description, or just ""
 }
